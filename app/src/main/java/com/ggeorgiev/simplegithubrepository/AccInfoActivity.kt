bb70ee1e394f4dad0396dc3ee2d1879474940c9c
gsgeorgiev89 <gsgeorgiev89@gmail.com>
2020-10-04 14:43:18 +0300

    First Commit
    First commit

---
+++ app/src/main/java/com/ggeorgiev/simplegithubrepository/AccInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.ggeorgiev.simplegithubrepository
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acc_info)
+    }
+}
